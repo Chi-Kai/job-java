552897e9cf59ee932ae536cc33a40c5778f27ded
chikai <chikai0307@gmail.com>
2021-01-25 21:20:22 +0800

    更新了部分代码

--- jobspridy.py
+++ jobspridy.py
@@ -4,6 +4,7 @@ import re
 import os
 import csv
 from  multiprocessing import Pool
+import time
 
 # 设置头部信息
 header = {
@@ -42,8 +43,8 @@ def get_data(url):
         
         Welfare = seletor.xpath("//div[@class='t1']/span/text()") # 如果所需多个标签的信息,可以获取该标签名的文本,会返回所有符合的内容
         req = seletor.xpath("//div[@class='bmsg job_msg inbox']/p/text()") # 查找所有 <p> 标签内容                 
+        key = ['job','wages','welfare','location','req'] # 设置列表头
         writer = csv.DictWriter(f,key)
-
         writer.writerow({'job':job,'wages':wages,'welfare':Welfare,'location':location,'req':req}) #以字典的形式写入文件
     
     except Exception:
@@ -65,5 +66,7 @@ if __name__ == "__main__" :
        url = f'https://search.51job.com/list/000000,000000,0000,00,9,99,java,2,{i}.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='
        urls = get_url(url,i)
       
-       with Pool(10) as p: #运用线程池,多线程获取数据
+       with Pool(15) as p: #运用线程池,多线程获取数据
            p.map(get_data,urls)# map函数的运用
+       for m in range(5):
+           time.sleep(m)
